872d3097983be61ccd9788914e407d81a6ef4590
basasha <tudi1815@gmail.com>
2017-07-27 15:51:06 +0800

    last edit 2017/07/26 17:58

--- QGraphicsTimeline.py
+++ QGraphicsTimeline.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from PyQt5.QtWidgets import (QGraphicsItem,
-    QGraphicsView, QGraphicsScene, QGraphicsRectItem, QDialog, QHBoxLayout,
-    QApplication, QGraphicsTextItem)
+    QGraphicsView, QGraphicsScene, QGraphicsRectItem, QDialog, QVBoxLayout,
+    QApplication, QGraphicsTextItem, QLabel)
 from PyQt5.QtCore import QPointF, Qt
 from PyQt5.QtGui import QColor, QPen, QPainter, QFont
 import sys
@@ -21,31 +21,50 @@ class QGraphicsTimeline(QGraphicsView):
         self.scene.setSceneRect(0, 0, 700, 700)
         #self.timeBlock =TimeBlock(None,length=20)
         #self.scene.addItem(self.timeBlock)
+        self.view = None#= QLabel("here show you what pos your mouse is",self)
         self.timeScale = TimeScale(None, 50, 50)
         #self.timeScale.setDragEnabled(True)
         self.scene.addItem(self.timeScale)
         self.currentDraggedItem = None
+        self.timeBlock = TimeBlock(None,50,100,50,50)
+        self.scene.addItem(self.timeBlock)
 
     def setSize(self, w, h):
         pass
 
     def setValue(self, value):
         self.value = value
-
-    
+    #@classmethod
+    def setOutput(self, o):
+        self.view = o
+    def setText(self, text):
+        if not self.view is None:
+            self.view.setText(text)
+                
     def mousePressEvent(self, event):
         self.currentDraggedItem = self.itemAt(event.pos())
-        super(QGraphicsTimeline, self).mousePressEvent(event)
-    
-    def mouseReleaseEvent(self, event):
         if not self.currentDraggedItem is None:
             c = self.currentDraggedItem
             p = c.parentItem()
             while p:
                 c=p
                 p=c.parentItem()
-            c.setPos(event.pos())
-        super(QGraphicsTimeline, self).mouseReleaseEvent(event)
+            r = self.currentDraggedItem.mapToParent(c.boundingRect().topLeft())
+            self.currentDraggedItem = c
+            self.point = event.pos() - r
+        self.setText('({}, {})'.format(event.pos().x(), event.pos().y()))    
+        #super(QGraphicsTimeline, self).mousePressEvent(event)
+    def mouseMoveEvent(self, event):
+        if not self.currentDraggedItem is None:
+            self.currentDraggedItem.setPos(event.pos()-self.point) 
+            self.setText('({}, {})'.format(event.pos().x(), event.pos().y()))
+
+    def mouseReleaseEvent(self, event):
+        if not self.currentDraggedItem is None:
+            self.currentDraggedItem.setPos(event.pos()-self.point)
+            self.setText('({}, {})'.format(event.pos().x(), event.pos().y()))
+            self.currentDraggedItem = None    
+        #super(QGraphicsTimeline, self).mouseReleaseEvent(event)
 
 
 
@@ -61,13 +80,16 @@ class TimeBlock(QGraphicsRectItem):
         super(TimeBlock, self).__init__(parent)
         self.w  = w  if w>50 else 50 # minimum width  = 50
         self.h = h if h>50 else 50  # minimum height = 50
+        self.x      = x
+        self.y      = y
         self.emotionPool =None
+        self.initUi()
     @classmethod
     def fromPath(cls, path):
         cls.emotionPool = path
 
 
-    def paintBlock(self):
+    def initUi(self):
         if not self.emotionPool is None:
             qp =QPainter()
             qp.begin(self)
@@ -84,7 +106,8 @@ class TimeBlock(QGraphicsRectItem):
                     d=d*0.05*self.h+0.1*self.h
                     qp.drawLine(int(i * self.w / timeFactor)-1,u,int(i * self.w  /timeFactor),u)
                     qp.drawLine(int(i * self.w / timeFactor)-1,d,int(i * self.w / timeFactor),d)
-                    
+        self.setRect(0,0,self.w,self.h)
+        self.setPos(self.x,self.y)            
 
 
 
@@ -102,7 +125,7 @@ class TimeScale(QGraphicsRectItem):
         self.start  = start
         self.baseline = 0
         #self.dragged =False
-        self.drawScale()
+        self.initUi()
     @classmethod
     def reSize(cls, w, h):
         cls.setSize(w, h)
@@ -115,9 +138,9 @@ class TimeScale(QGraphicsRectItem):
     def setBaseLine(self, b):
         self.baseline = b
 
-    def drawScale(self):
+    def initUi(self):
         
-        pos = QPointF(self.x, self.y)
+        pos = QPointF(0, 0)
 
         for i in range(0, self.length):#每一秒有1個刻度，總共 length 秒
             t1 = QGraphicsTextItem('|', self)
@@ -129,16 +152,20 @@ class TimeScale(QGraphicsRectItem):
             t2.setPos(t1.boundingRect().bottomLeft()+pos)
 
             pos.setX(pos.x() + int(self.w/ self.length))
-        self.setRect(50,50,self.w, self.h)
-  
+        self.setRect(0, 0, self.w, self.h)
+        #self.setPos(50,50)
 
 
 
 class MainFrame(QDialog):
     def __init__(self, parent = None):
         super(MainFrame, self).__init__(parent)
-        layout = QHBoxLayout(self)
-        layout.addWidget(QGraphicsTimeline())
+        layout = QVBoxLayout(self)
+        self.v = QLabel("here show you what pos your mouse is",self)
+        layout.addWidget(self.v)
+        self.g = QGraphicsTimeline()
+        self.g.setOutput(self.v)
+        layout.addWidget(self.g)
         self.setWindowTitle("Basic test")
 
 def main():                      
@@ -151,6 +178,7 @@ def main():
 if __name__ == '__main__':
     main()
 '''
-2017/07/26 13:58-
+2017/07/26 13:58-18:10
+2017/07/27 13:50-
 
 '''
